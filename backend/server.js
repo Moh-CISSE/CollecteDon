9ad86f914223779b5aeedeef6f9ef8f589a4f0c9
Moh-CISSE <mohamedcisse8484@gmail.com>
2026-04-29 01:04:02 +0200

    test connexion mysql

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
+import express from "express";
+import { db } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+try {
+  const connection = await db.getConnection();
+  console.log("Connected to MySQL");
+  connection.release();
+} catch (err) {
+  console.error("Erreur de connexion MySQL :", err);
+}
 
 app.use(cors());
 app.use(express.json());
